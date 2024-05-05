8fa18866e5b8f0b5cfe5668f77e960f7b2127bc5
gaabsito <a23185@svalero.com>
2024-05-05 14:01:06 +0200

    act carta, ajuste anim. y cosas 2

--- JS/menu.js
+++ JS/menu.js
@@ -58,19 +58,4 @@
 
 
 
-    // Función para cambiar la clase activa en las secciones
-    function setActiveSection() {
-      var sections = document.querySelectorAll('.section');
-      sections.forEach(function(section) {
-          var rect = section.getBoundingClientRect();
-          if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-              section.classList.add('active');
-          } else {
-              section.classList.remove('active');
-          }
-      });
-  }
 
-  // Llama a setActiveSection al cargar y al hacer scroll
-  window.addEventListener('load', setActiveSection);
-  window.addEventListener('scroll', setActiveSection);
